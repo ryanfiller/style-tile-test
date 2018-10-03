0e4ac89164c36fb802098794caeacd19e06990fb
Ryan Filler <ryanfiller89@gmail.com>
2018-10-03 17:11:24 -0500

    initial setup stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,17 +5,25 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
+			resolve: `gatsby-plugin-netlify-cms`,
+			options: {
+				// One convention is to place your Netlify CMS customization code in a
+				// `src/cms` directory.
+				// modulePath: `${__dirname}/src/cms/cms.js`,
+			},
+		},
+    {
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `content`,
+				path: `${__dirname}/src/content/`,
+			},
     },
-    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+
+      }
+    }
   ],
 }
